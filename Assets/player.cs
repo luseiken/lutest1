e505ca52129f4c96a19fdc47b0cfcc7f1f85917a
luseiken <33753965+luseiken@users.noreply.github.com>
2018-02-02 22:41:34 +0800

    改用頭驅動身體

--- Assets/player.cs
+++ Assets/player.cs
@@ -3,45 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour {
-	public float speed=0.03f;
-	public float jump=1.5f;
 
+GameObject head;
 
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("HELLO");
+		head = GameObject.Find("head");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		if (Input.GetKey ("w"))
-		{
-			GetComponent<Rigidbody>().velocity+=transform.forward*0.5f;
-		}
-		if (Input.GetKey ("s"))
-		{
-			GetComponent<Rigidbody>().velocity=-transform.forward*(1+speed);
-		}
-		if (Input.GetKey ("a"))
-		{
-			GetComponent<Rigidbody>().velocity=new Vector3(-speed,0,0);
-		}
-		if (Input.GetKey ("d"))
-		{
-			GetComponent<Rigidbody>().velocity=new Vector3(speed,0,0);
-		}
-		if (Input.GetKeyDown ("space"))
-		{
-			GetComponent<Rigidbody>().AddForce(new Vector3(0,jump,0));
-		}
-		if (Input.GetKey ("e"))
-		{
-			transform.Rotate (0, 1, 0);
-		}
-		if (Input.GetKey ("q"))
-		{
-			transform.Rotate (0, -1, 0);
-		}
+ transform.position = head.transform.position - transform.forward;
+		
 	}
 }
--- Assets/playerHead.cs
+++ Assets/playerHead.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class playerHead : MonoBehaviour {
 
-	GameObject player;// Use this for initialization
+	// Use this for initialization
+    public float speed=0.03f;
+	public float jump=1.5f;
 	void Start () {
-		player = GameObject.Find("player");
+		
 	}
 	
 	// Update is called once per frameu
 	void Update () {
-		transform.position = player.transform.position + transform.forward;
+		
+		if (Input.GetKey ("w"))
+		{
+			GetComponent<Rigidbody>().velocity+=transform.forward*(0.25f+speed);
+		}
+		if (Input.GetKey ("s"))
+		{
+			GetComponent<Rigidbody>().velocity-=transform.forward*(0.25f+speed);
+		}
+		if (Input.GetKey ("a"))
+		{
+			GetComponent<Rigidbody>().velocity+=new Vector3(-0.25f+speed,0,0);
+		}
+		if (Input.GetKey ("d"))
+		{
+			GetComponent<Rigidbody>().velocity+=new Vector3(0.25f+speed,0,0);
+		}
+		if (Input.GetKeyDown ("space"))
+		{
+			GetComponent<Rigidbody>().AddForce(new Vector3(0,30+jump,0));
+		}
+		if (Input.GetKey ("e"))
+		{
+			transform.Rotate (0, 1, 0);
+		}
+		if (Input.GetKey ("q"))
+		{
+			transform.Rotate (0, -1, 0);
+		}
 	}
 }
